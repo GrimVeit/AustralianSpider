93b4cdb1cba2ef5bcaa26f38e25ed7413df1e7c1
Denis Khasanov <denishasanov828@gmail.com>
2025-02-19 19:58:42 +0500

    0.1.5.3

---
+++ AustralianSpider/Assets/Scripts_Project/Cards/StoreCard/StoreCardPresenter.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreCardPresenter : MonoBehaviour
+{
+    private StoreCardModel storeCardModel;
+
+    public StoreCardPresenter(StoreCardModel storeCardModel)
+    {
+        this.storeCardModel = storeCardModel;
+    }
+
+    public void Initialize()
+    {
+
+    }
+
+    public void Dispose()
+    {
+
+    }
+
+    #region Input
+
+    public void SetCoverCardDesign(CoverCardDesign coverCardDesign)
+    {
+
+    }
+
+    public void SetFaceCardDesign(FaceCardDesign faceCardDesign)
+    {
+
+    }
+
+    public void SetGameType(GameType gameType)
+    {
+
+    }
+
+    #endregion
+}
